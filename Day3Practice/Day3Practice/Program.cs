e7f837d1c18f282319f43d7e7f24f50da4cd1feb
Eric Fowler <ericfowler303@gmail.com>
2014-10-29 20:46:46 -0600

    Day 3 Stuff plus the homework FlipMania

---
+++ Day3Practice/Day3Practice/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LetterFinder('e', "Where is my car dude");
+            WordFinder("test", "When do we test the code?");
+            // keep the console open
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Checks for the given letter inside of a string
+        /// </summary>
+        /// <param name="letter">input letter</param>
+        /// <param name="inputString">input string</param>
+        static void LetterFinder(char letter, string inputString)
+        {
+            int letterCounter = 0;
+            // check to make sure the char is actually a letter in the alphabet
+            if(ContainsValidLetters(letter.ToString())) {
+                // iterate through each letter
+                foreach (char testLetter in inputString)
+                {
+                    // check to see if the character matches and if it does increment the counter
+                    if (letter.ToString().ToLower() == testLetter.ToString().ToLower()) letterCounter++;
+                }
+                Console.WriteLine(inputString + " has " + letterCounter + " instances of letter " + letter.ToString());
+            }
+            else { Console.WriteLine("The letter given is not a letter"); }
+            
+        }
+
+        /// <summary>
+        /// Checks for the given word inside of a string
+        /// </summary>
+        /// <param name="word">input word</param>
+        /// <param name="inputString">input string</param>
+        static void WordFinder(string word, string inputString)
+        {
+            int wordCounter = 0;
+            // Split the string into individual words and put each into the list
+            List<string> wordList = inputString.Split(' ').ToList();
+            // loop through the words in the list
+            foreach(string testWord in wordList)
+            {
+                // if the words match increment the counter
+                if (word.ToLower() == testWord.ToLower() && ContainsValidLetters(word.ToLower())) wordCounter++;
+            }
+
+            // Check for valid word to create the proper output statement
+            if (ContainsValidLetters(word.ToLower()))
+            {
+                Console.WriteLine(inputString + " has " + wordCounter + " instances of " + word);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input word");
+
+            }
+        }
+
+        /// <summary>
+        /// Check for valid alphabetical letters in a string
+        /// </summary>
+        /// <param name="word">input word</param>
+        /// <returns>true if all letters are valid</returns>
+        static bool ContainsValidLetters(string word)
+        {
+            bool valid = false;
+           foreach(char letter in word)
+           {
+               
+               if ("abcdefghijklmnopqrstuvwxyz".Contains(letter.ToString())) { valid = true;} else { return false;}
+               
+           }
+           return valid;
+        }
+    }
+}
---
+++ FlipMania/FlipMania/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoins(10000);
+            FlipForHeads(10000);
+
+            // keep the console open
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Flip a coin a number of times and write the result to the console
+        /// </summary>
+        /// <param name="numberOfFlips">how many coin flips</param>
+        static void FlipCoins(int numberOfFlips)
+        {
+            // Check for valid input
+            if (IsValidInt(numberOfFlips))
+            {
+                int numberOfHeads = 0;
+                int numberOfTails = 0;
+                Random rng = new Random();
+
+                // Flip a number of times
+                for (int i = 0; i < numberOfFlips; i++)
+                {
+                    // Check if heads or tails and increment the respective counter
+                    if (rng.Next(2) == 0) { numberOfHeads++; } else { numberOfTails++; }
+                }
+
+                // Write out the results to the console
+                Console.WriteLine("We flipped a coin " + numberOfFlips + " times.");
+                Console.WriteLine("Number of Heads: " + numberOfHeads);
+                Console.WriteLine("Number of Tails: " + numberOfTails);
+            }
+        }
+
+        static void FlipForHeads(int numberOfHeads)
+        {
+            // Check for valid input
+            if (IsValidInt(numberOfHeads))
+            {
+                int numberOfHeadsFlipped = 0;
+                int totalFlips = 0;
+                Random rng = new Random();
+
+                // Flip until there are enough heads found
+                while (numberOfHeadsFlipped <= numberOfHeads)
+                {
+                    // increment if it is a head that is found
+                    if (rng.Next(2) == 0) { numberOfHeadsFlipped++; }
+                    // totalFlips gets incremented regardless of the result
+                    totalFlips++;
+                }
+
+                // Write the results to the console
+                Console.WriteLine("We are flipping a coin until we find " + numberOfHeads + " heads");
+                Console.WriteLine("It took " + totalFlips + " to find " + numberOfHeads + " heads");
+            }
+        }
+
+        /// <summary>
+        /// Check to see if the integer is a positive number that's not zero
+        /// </summary>
+        /// <param name="someInt">integer to check</param>
+        /// <returns>true if valid positive number</returns>
+        static bool IsValidInt(int someInt)
+        {
+            if (someInt > 0) { return true; } else { return false; }
+        }
+    }
+}
