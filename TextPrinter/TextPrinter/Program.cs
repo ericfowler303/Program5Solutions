ca2bd135d1e0120d24f34559387b5cf37dccfa15
Eric Fowler <ericfowler303@gmail.com>
2014-11-06 16:22:51 -0700

    Examples using Lambdas

---
+++ TextPrinter/TextPrinter/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace TextPrinter
+{
+    class Program
+    {
+        public static int textIndexCounter=0;
+        public static string textString = "";
+        public static Timer myTimer;
+        static void Main(string[] args)
+        {
+            TextPrinter("print me out really slow", 200);
+        }
+
+        /// <summary>
+        /// Print out a line of text a certain speed
+        /// </summary>
+        /// <param name="text">string of text to print</param>
+        /// <param name="mSecDelay">delay in miliseconds between each character is printed</param>
+        static void TextPrinter(string text, double mSecDelay)
+        {
+            // Assign the text to a public available string
+            textString = text;
+            // Set the text counter for the TickEvent
+            textIndexCounter = text.Length;
+            // Create a Timer to send off Tick events
+            myTimer = new Timer(mSecDelay);
+            myTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            myTimer.AutoReset = false;
+
+            // Start the timer
+            myTimer.Enabled = true;
+
+            Console.ReadKey();
+            
+        }
+        /// <summary>
+        /// A function that handles what needs to be done during each Tick from the Timer
+        /// </summary>
+        private static void OnTimedEvent(object obj, ElapsedEventArgs e)
+        {
+            if (textIndexCounter < textString.Length)
+            {
+                Console.Write(textString[textIndexCounter]);
+                textIndexCounter++;
+            }
+            else
+            {
+                // At the end of the string stop the timer
+                myTimer.Stop();
+            }
+        }
+    }
+}
---
+++ Week2LambdaPractice/Week2LambdaPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2LambdaPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunTime();
+
+            Console.ReadKey();
+        }
+        static void FunTime()
+        {
+            List<string> funtimeList = new List<string>() { "blueberry", "boisonberry", "raspberry", "apple", "pear", "orange"};
+            // print out in order
+            Console.WriteLine("In Order: "+string.Join(", ", funtimeList.OrderBy(x => x)));
+            // print only the ones with berry
+            Console.WriteLine("Contains \"berry\": " +string.Join(", ",funtimeList.Where(x => x.Contains("berry"))));
+
+            // print on the non berry items, ordered by length of the word
+            Console.WriteLine("non berry, by length: " + string.Join(", ", funtimeList.Where(x => !x.Contains("berry")).OrderBy(x =>x.Length)));
+
+            // print only the items that have 5 or more characters
+            Console.WriteLine("more then 5 characters: "+string.Join(", ", funtimeList.Where(x=>x.Length>5)));
+
+            // Find average number of characters in the list
+            Console.WriteLine("average number of characters: " + funtimeList.Average(x => x.Length));
+
+            // Find the number of vowels in each item in the list
+            Console.WriteLine("number of vowels in each word: " + string.Join(", ", funtimeList.Select(x=> x.Count(y=> "aeiou".Contains(y)))));
+        }
+    }
+}
